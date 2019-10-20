aa5738275981a4507f6fd926f1022645d5636c08
Çağatay YILDIZ <cagatay.yildiz@baogrenci.com>
2019-10-20 16:06:47 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports ={
+    sql:{
+        user: 'kctakffomsgtvd',
+        host: 'ec2-54-83-55-122.compute-1.amazonaws.com',
+        database: 'deatc97mcr5big',
+        password: 'e6be9623e524a88d3265060ab660dd2fafa5d27cb093fbd7fe1e7b649f0be52f',
+        port: 5432,
+        ssl: true,
+    },
+    localsql:{
+        user: 'postgres',
+        host: 'localhost',
+        database: 'MayinTarlasiDB',
+        password: '123',
+        port: 5432,
+    }
+}
